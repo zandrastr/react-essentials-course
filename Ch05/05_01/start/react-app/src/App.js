5e08c1d86004b9abc2421cc8df9e8278b6f4ad76
Sandra S <zandrastr@hotmail.com>
2024-02-28 17:19:31 +0000

    create controlled form elements

--- Ch05/05_01/start/react-app/src/App.js
+++ Ch05/05_01/start/react-app/src/App.js
@@ -1,23 +1,21 @@
 import "./App.css";
-import { useRef } from "react";
+import { useState } from "react";
 
 function App() {
-  const textTitle = useRef();
-  const hexColor = useRef();
+  const [title, setTitle] = useState('');
+  const [color, setColor] = useState('#000000');
 
   const submit = (e) => {
     e.preventDefault();
-    const title = textTitle.current.value;
-    const color = hexColor.current.value;
-    alert(`${title}, ${color}`)
-    textTitle.current.value = '';
-    hexColor.current.value = '';
+    alert(`${title}, ${color}`);
+    setTitle('');
+    setColor('#000000');
   }
 
   return (
     <form onSubmit={submit}>
-      <input ref={textTitle} type="text" placeholder="Color title..."></input>
-      <input ref={hexColor} type="color"></input>
+      <input value={title} onChange={(event) => setTitle(event.target.value)} type="text" placeholder="Color title..."></input>
+      <input value={color} onChange={(event) => setColor(event.target.value)}  type="color"></input>
       <button>ADD</button>
     </form>
 
