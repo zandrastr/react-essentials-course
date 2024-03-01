8bdbb396bcc6f01908eddcaa23dafe2e4eb7b299
Sandra S <zandrastr@hotmail.com>
2024-03-01 14:15:44 +0000

    fetch data with hooks, github api

--- Ch06/06_01/start/react-app/src/App.js
+++ Ch06/06_01/start/react-app/src/App.js
@@ -1,41 +1,22 @@
 import "./App.css";
-import { useState } from "react";
-
-function useInput(initialValue) {
-  const [value, setValue] =
-    useState(initialValue);
-  return [
-    {
-      value,
-      onChange: (e) => setValue(e.target.value)
-    },
-    () => setValue(initialValue)
-  ];
-}
+import { useState, useEffect } from "react";
 
 function App() {
-  const [titleProps, resetTitle] = useInput("");
-  const [colorProps, resetColor] =
-    useState("#000000");
-  const submit = (e) => {
-    e.preventDefault();
-    alert(
-      `${titleProps.value}, ${colorProps.value}`
-    );
-    resetTitle();
-    resetColor();
-  };
+  const [data, setData] = useState(null);
+  
+  useEffect(() => {
+    fetch(
+      'https://api.github.com/users/zandrastr'
+    )
+    .then((resonse ) => resonse.json())
+    .then(setData)
+  }, []);
+
+  if (data) return (<pre>{JSON.stringify(data, null, 2)}</pre>)
+
   return (
-    <form onSubmit={submit}>
-      <input
-        {...titleProps}
-        type="text"
-        placeholder="color title..."
-      />
-      <input {...colorProps} type="color" />
-      <button>ADD</button>
-    </form>
+    <h1>Data</h1>
   );
-}
+};
 
 export default App;
