766549727e6043986b062e2c262bd7c6568d3994
Sandra S <zandrastr@hotmail.com>
2024-03-01 14:28:03 +0000

    handle loading states

--- Ch06/06_01/start/react-app/src/App.js
+++ Ch06/06_01/start/react-app/src/App.js
@@ -15,27 +15,31 @@ function GithubUser({name, location, avatar}) {
 
 function App() {
   const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
   
   useEffect(() => {
+    setLoading(true);
     fetch(
       'https://api.github.com/users/zandrastr'
     )
     .then((resonse ) => resonse.json())
     .then(setData)
+    .then(() => setLoading(false))
+    .catch(setError);
   }, []);
 
-  if (data) 
-    return (
-      <GithubUser 
-      name={data.name} 
-      location={data.location}
-      avatar={data.avatar_url}
-      />
-    )
+  if(loading) return <h1>Loading...</h1>
+  if(error) return <pre>{JSON.stringify(error)}</pre>
+  if(!data) return null;
 
   return (
-    <h1>Data</h1>
-  );
+    <GithubUser 
+    name={data.name} 
+    location={data.location}
+    avatar={data.avatar_url}
+    />
+  )
 };
 
 export default App;
